645562d47b1477ef608f6e2c000c5a520d2c3c00
Murilo Carnelos <muriloc.c@hotmail.com>
2022-10-24 22:07:05 -0300

    knex, notesRoutes and NotesController

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,70 @@
+const knex = require("../database/knex")
+
+class NotesController{
+  async create(request, response) {
+    const { title, description, rating, tags } = request.body
+    const{ user_id } = request.params
+
+    const note_id = await knex("movie_notes").insert({
+      title,
+      description,
+      rating,
+      user_id
+    })
+
+    const tagsInsert = tags.map(name => {
+      return {
+        note_id,
+        name,
+        user_id
+      }
+    })
+
+    await knex("movie_tags").insert(tagsInsert)
+
+    response.json()
+  }
+
+  async show(request, response) {
+    const { id } = request.params
+
+    const note = await knex("movie_notes").where({ id }).first()
+    const tags = await knex("movie_tags").where({ note_id: id }).orderBy("name")
+
+    return response.json({
+      ...note,
+      tags
+    })
+  }
+
+  async delete(request, response) {
+    const { id } = request.params
+
+    await knex("movie_notes").where({ id }).delete()
+
+    return response.json()
+  }
+
+  async index(request, response) {
+    const { title, user_id, tags } = request.query
+
+    let notes
+
+    if(tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim())
+
+      notes = await knex("movie_tags")
+      .whereIn("name", filterTags)
+    
+    } else {
+      notes = await knex("movie_notes")
+      .where({ user_id })
+      .whereLike("title", `%${title}%`)
+      .orderBy("title")
+    }
+
+    return response.json(notes)
+  }
+}
+
+module.exports = NotesController
---
+++ src/routes/notes.routes.js
@@ -0,0 +1,16 @@
+const { Router } = require("express")
+
+const NotesController = require("../controllers/NotesController")
+
+const notesRoutes = Router()
+
+//instanciando a classe
+const notesController = new NotesController()
+
+//rotas
+notesRoutes.get("/", notesController.index)
+notesRoutes.post("/:user_id", notesController.create)
+notesRoutes.get("/:id", notesController.show)
+notesRoutes.delete("/:id", notesController.delete)
+
+module.exports =  notesRoutes
